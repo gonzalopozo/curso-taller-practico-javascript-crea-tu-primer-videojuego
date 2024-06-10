818bf87e7ae2c903e0f1cabb4b27fb97ce09a688
gonzalopozo <gonzalopozo2005@gmail.com>
2024-06-10 18:13:59 +0200

    Tamaño del canvas y sus elementos

---
+++ game.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector("#game");
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    let canvasSize;
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementsSize = canvasSize / 13;
+
+    console.log({ canvasSize, elementsSize });
+
+    game.font = elementsSize + "px Verdana";
+    game.textAlign = "end";
+
+    for (let i = 0; i <= 10; i++) {
+        game.fillText(emojis["X"], (canvasSize / 10) * i, elementsSize);
+    }
+    
+}
