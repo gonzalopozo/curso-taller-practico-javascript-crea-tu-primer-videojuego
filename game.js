771297cb4f9c907e5c764f69759abb01de578a4f
gonzalopozo <gonzalopozo2005@gmail.com>
2024-06-12 18:01:33 +0200

    Refactor del mapa del juego juntos a los botones y las teclas con sus respectivos eventos

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
@@ -29,16 +33,75 @@ function startGame() {
     game.font = `${elementsSize}px Verdana`;
     game.textAlign = "start";
 
-    const map = maps[0];
-    let mapRows = map.trim().split('\n');
-    let mapRowsCols = mapRows.map(row => row.trim().split(''));
+    const map = maps[1];
+    const mapRows = map.trim().split('\n');
+    const mapRowsCols = mapRows.map(row => row.trim().split(''));
     console.log({ map, mapRows, mapRowsCols });
 
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 0; col < 10; col++) {
-            game.fillText(emojis[mapRowsCols[row - 1][col]], elementsSize * col, elementsSize * row);
-        }
-    }       
+    mapRowsCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI);
+            const posY = elementsSize * (rowI + 1);
+            game.fillText(emoji, posX, posY);
+            console.log({ row, rowI, col, colI });
+        })
+    });
+
+    // for (let row = 1; row <= 10; row++) {
+    //     for (let col = 0; col < 10; col++) {
+    //         game.fillText(emojis[mapRowsCols[row - 1][col]], elementsSize * col, elementsSize * row);
+    //     }
+    // }       
 }
 
+window.addEventListener('keydown', (e) => {
+    const tecla = e.key;
 
+    const moveFunctions = {
+        ArrowUp: moveUp,
+        ArrowLeft: moveLeft,
+        ArrowRight: moveRight,
+        ArrowDown: moveDown
+    }
+
+    const moveFunction = moveFunctions[tecla];
+
+    if (moveFunction) {
+        moveFunction();
+    }
+});
+
+// 1º Forma de la que se hizo.
+// 
+// windows.addEventListener('keydown', moveByKeys);
+// 
+// function moveByKeys(event) {
+//     if (event.key == 'ArrowUp') moveUp();
+//     else if (event.key == 'ArrowLeft') moveLeft();
+//     else if (event.key == 'ArrowRight') moveRight();
+//     else if (event.key == 'ArrowDown') moveDown();
+// }
+
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+
+
+function moveUp() {
+    console.log("¡Me quiero mover hacia arriba!");
+}
+
+function moveLeft() {
+    console.log("¡Me quiero mover hacia la izquierda!");
+}
+
+function moveRight() {
+    console.log("¡Me quiero mover hacia la derecha!");
+}
+
+function moveDown() {
+    console.log("¡Me quiero mover hacia abajo!");
+}
